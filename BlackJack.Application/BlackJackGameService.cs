e31d211e0d98f70ee77bc76d08ffe2e23b732675
stefan.sz <stefan.szakal@clusterfig.com>
2020-01-06 11:29:09 +0000

    remove tracked files from gitignore

--- BlackJack.Application/BlackJackGameService.cs
+++ BlackJack.Application/BlackJackGameService.cs
@@ -7,11 +7,11 @@ namespace BlackJack.Application
 {
     public class BlackJackGameService: IBlackJackGameService
     {
-        private readonly Dictionary<Guid, BlackJackGame> _game;
+        private readonly Dictionary<Guid, BlackJackGame> _games;
 
         public BlackJackGameService()
         {
-            _game = new Dictionary<Guid, BlackJackGame>();
+            _games = new Dictionary<Guid, BlackJackGame>();
         }
 
         public Guid CreateGame(string playerName)
@@ -20,18 +20,18 @@ namespace BlackJack.Application
             var deck = new Deck(new RandomNumberGenerator());
             var dealer = new BlackJackDealer();
             var player = new BlackJackPlayer(playerName);
-            _game[newId] = new BlackJackGame(newId, deck, dealer, player);
+            _games[newId] = new BlackJackGame(newId, deck, dealer, player);
             return newId;
         }
 
         public BlackJackGame[] GetAllGames()
         {
-            return _game.Values.ToArray();
+            return _games.Values.ToArray();
         }
 
         public BlackJackGame GetGameById(Guid id)
         {
-            return _game.ContainsKey(id) ? _game[id]:null;
+            return _games.ContainsKey(id) ? _games[id]:null;
         }
     }
 }
--- BlackJack.Domain/Values.cs
+++ BlackJack.Domain/Values.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace BlackJack.Domain
+﻿namespace BlackJack.Domain
 {
     public enum Values
     {
